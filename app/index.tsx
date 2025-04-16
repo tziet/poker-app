5cbf0903430e8ddc4efe91800e34a5f0a1f99a93
yonatan tseitlin <yoni.tseitlin@gmail.com>
2025-04-17 02:08:20 +0300

    links

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">Poker!</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      {/*<Link href="/player/Yonatan">Yonatan</Link>*/}
     </View>
   );
 }
