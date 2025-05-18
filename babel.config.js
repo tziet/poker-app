2259c0e3855f1d343978acd1b4afc16538d677b6
yonatan tseitlin <yoni.tseitlin@gmail.com>
2025-05-18 15:55:00 +0300

    Sfghds

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,16 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "^react-native$": "react-native",
+          },
+        },
+      ],
+    ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,14 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Add additional resolver for React Native
+config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs"];
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  "react-native": require.resolve("react-native"),
+};
+
+// Add support for Hermes
+config.transformer.unstable_allowRequireContext = true;
+
 module.exports = withNativeWind(config, { input: "./app/globals.css" });
